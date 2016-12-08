50bf87a22a76cadc72d1f86883540587092480b2
Lam Le <lam281990@gmail.com>
2016-12-08 12:59:01 -0800

    Improve the speed by derialize them first

--- src/SortableHiringChallenges/Program.cs
+++ src/SortableHiringChallenges/Program.cs
@@ -15,34 +15,32 @@ namespace SortableHiringChallenges
             var productInputRaw = File.ReadLines(args[0]);
             var listingInputRaw = File.ReadLines(args[1]);
 
-            var productsInput = productInputRaw.Select(JsonConvert.DeserializeObject<Product>);
+            var productsInput = productInputRaw.Select(JsonConvert.DeserializeObject<Product>).ToList();
             var listingsInput = listingInputRaw.Select(JsonConvert.DeserializeObject<Listing>);
 
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            var prodGroupingQuery = from prod in productsInput
+                                    group prod by prod.Manufacturer into groupByManufacturer
+                                    from prodInGroupByManufacturer in (from prod in groupByManufacturer
+                                                                       group prod by prod.Family ?? "NoFamily")
+                                    group prodInGroupByManufacturer by groupByManufacturer.Key;
+
             var productManufacturers = productsInput.GroupBy(prod => prod.Manufacturer).ToList();
-
-            var prodGrouping = from prod in productsInput
-                               group prod by prod.Manufacturer into groupByManufacturer
-                               from prodInGroupByManufacturer in (from prod in groupByManufacturer
-                                                                  group prod by prod.Family ?? "NoFamily")
-                               group prodInGroupByManufacturer by groupByManufacturer.Key;
-            var productLookup = prodGrouping.ToDictionary(groupByManufacturer => groupByManufacturer.Key,
-                prodByManufacturer => prodByManufacturer.ToDictionary(groupByFamily => groupByFamily.Key, prod => prod.ToList()));
-
             var listingGroupedByManufacturers =
-                listingsInput.GroupBy(
-                        listing =>
+                listingsInput.GroupBy(listing =>
                             productManufacturers.FirstOrDefault(m => listing.Manufacturer.ToLower().Replace(" ", "")
                                                                            .Contains(m.Key.ToLower().Replace(" ", ""))
                             )?.Key ?? "Other")
                 .Where(group => group.Key != "Other");
 
 
-            var results = (from productGroup in productLookup
+            var results = (from productGroup in prodGroupingQuery
                            let listingByCurrentManufacturer = listingGroupedByManufacturers.Where(listingGroup => listingGroup.Key == productGroup.Key).SelectMany(prod => prod).ToList()
-                           from productGroupByFamily in productGroup.Value
+                           from productGroupByFamily in productGroup
                            let familyRegexKey = productGroupByFamily.Key.Replace("-", "-*").Replace(" ", " *")
                            let listingForThisFamily = listingByCurrentManufacturer.Where(listing => Regex.IsMatch(listing.Title, familyRegexKey, RegexOptions.IgnoreCase)).ToList()
-                           from product in productGroupByFamily.Value
+                           from product in productGroupByFamily
                            let modelRegexKey = product.Model.Replace("-", "-*").Replace(" ", " *")
                            let listingForThisProduct = listingForThisFamily.Where(listing => Regex.IsMatch(listing.Title, modelRegexKey, RegexOptions.IgnoreCase)).ToList()
                            select new Result()
@@ -50,6 +48,9 @@ namespace SortableHiringChallenges
                                ProductName = product.ProductName,
                                Listings = listingForThisProduct
                            }).ToList();
+
+            stopwatch.Stop();
+
 
             File.WriteAllLines("results.txt", results.Select(JsonConvert.SerializeObject));
         }
